3f71f35544c31e4a4c2847e0ac1884dc9d5e34b1
dltjd <dltjd@LAPTOP-9D7VMTLV>
2019-01-08 17:19:39 +0900

    post

--- spring_base_project_1/src/main/java/com/spring/controller/PostController.java
+++ spring_base_project_1/src/main/java/com/spring/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,12 @@ public class PostController {
 
 	@GetMapping("/list")
 	public Object postListGET() {
+
+		return null;
+	}
+
+	@PostMapping("")
+	public Object insertPost() {
 
 		return null;
 	}
