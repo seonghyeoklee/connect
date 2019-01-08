8266d58eb7a6037ab0e6d4df52708f2c7dee2c37
SeongHyeok_Lee <dltjdgur327@gmail.com>
2019-01-08 22:42:30 +0900

    kakao_admin_key

--- spring_base_project_1/src/main/java/com/spring/common/Constant.java
+++ spring_base_project_1/src/main/java/com/spring/common/Constant.java
@@ -6,6 +6,6 @@ public class Constant {
 
 	public final static String COOKIE_LOGIN = "COOKIE_LOGIN";
 
-	public final static String KAKAO_ADMIN_KEY = "9d494a36270630d16f3db17fb25ffa1c";
+	public final static String KAKAO_ADMIN_KEY = "";
 
 }
