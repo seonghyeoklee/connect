ba56634d8d000bea3c3ec967739f38dd5a01dc50
SeongHyeok_Lee <dltjdgur327@gmail.com>
2019-01-17 21:16:24 +0900

    mailUtil

---
+++ spring_base_project_1/src/main/java/com/spring/util/MailUtil.java
@@ -0,0 +1,41 @@
+package com.spring.util;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class MailUtil {
+	public boolean send(String fromName,String to,String title,String content){
+		//보내는사람 정보
+		String host = "smtp.daum.net";
+		String username = "";
+		String password = "";
+
+		Properties props = new Properties();
+		props.put("mail.smtps.auth", "true");
+
+		Session session = Session.getDefaultInstance(props);
+		MimeMessage msg = new MimeMessage(session);
+		try{
+			// 메일 관련
+			msg.setSubject(title,"utf-8");
+			msg.setContent(content, "text/html;charset=utf-8");
+			msg.setFrom(new InternetAddress(username,fromName));
+			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+			// 발송 처리
+			Transport transport = session.getTransport("smtps");
+			transport.connect(host, username, password);
+			transport.sendMessage(msg, msg.getAllRecipients());
+			transport.close();
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+}
